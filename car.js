90556148cc1cfbe9d7aa58126916d2fd2e4e64c3
gigg88 <popp.christian88@gmail.com>
2024-09-14 10:08:41 +0300

    x

---
+++ car.js
@@ -0,0 +1,8 @@
+// Based on the example from the previous lesson, you can work directly in the browser or create separate HTML/JavaScript files – choose a more convenient option. 
+
+// Create an object that describes a car (manufacturer, model, year of release, average speed) and the following methods for working with this object:
+
+// A method that displays the car info 
+// A method for counting a time frame necessary to cover a given distance with the average speed. Note that every 4 hours the driver has to take a 1-hour break.
+
+
