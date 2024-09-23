1549dc3a3a6b4487e12d84bc24b388f9e2d3a55e
gigg88 <popp.christian88@gmail.com>
2024-09-23 17:18:53 +0300

    x

--- car.js
+++ car.js
@@ -1,8 +1,33 @@
-// Based on the example from the previous lesson, you can work directly in the browser or create separate HTML/JavaScript files – choose a more convenient option. 
+// Based on the example from the previous lesson, you can work directly in the browser or create
+// separate HTML/JavaScript files – choose a more convenient option. 
 
-// Create an object that describes a car (manufacturer, model, year of release, average speed) and the following methods for working with this object:
+// Create an object that describes a car (manufacturer, model, year of release, average speed) and 
+// the following methods for working with this object:
 
 // A method that displays the car info 
-// A method for counting a time frame necessary to cover a given distance with the average speed. Note that every 4 hours the driver has to take a 1-hour break.
+// A method for counting a time frame necessary to cover a given distance with the average speed.
+// Note that every 4 hours the driver has to take a 1-hour break.
+
+const car = {
+    manufacturer: "Toyota",
+    model: "Camry",
+    yearOfRelease: 2023,
+    averageSpeed: 120,
+    displayInfo: function() {
+      console.log("Manufacturer:", this.manufacturer);
+      console.log("Model:", this.model);
+      console.log("Year of Release:", this.yearOfRelease);
+      console.log("Average Speed:", this.averageSpeed);  
+    },
+    timeFrame: function(distance) {
+        const journey = distance / this.averageSpeed;
+        console.log(journey);
+    }
+  };
+
+
+
+  car.displayInfo();
+  car.timeFrame(1000);
 
 
